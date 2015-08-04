6a67772c30e5274e89dfbccd41592086cf429489
skyitachi <skbysp@gmail.com>
2015-08-04 18:57:35 +0800

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require("http");
 var fs = require("fs");
 var path = require("path");
 var execSync = require("child_process").execSync;
+var HTTPStatus = require("http-status");
 
 function parseUrl(url) {
     var matches = /^\/node\/(\d+)\/([M,A,D])\/?$/.exec(url);
@@ -14,29 +15,76 @@ function parseUrl(url) {
     return {};
 }
 
-function modifyContent(pathname, content) {
-    fs.writeFileSync(pathname, content);
+function modifyContent(res, pathname, content) {
+    try {
+        fs.writeFileSync(pathname, content);
+    } catch (e) {
+        var error = parseError(e);
+        respond(res, error.code, error.msg);        
+        return;
+    }
+    respond(res, 200, "OK");
 }
 
 function addContent(pathname, content) {
     var dirname = path.dirname(pathname);
-    if (fs.existsSync(dirname)) {
-        fs.writeFileSync(pathname, content);
-    } else {
-        fs.mkdirSync(dirname, "0644");
-        fs.writeFileSync(pathname, content)
+    try {
+        if (fs.existsSync(dirname)) {
+            fs.writeFileSync(pathname, content);
+        } else {
+            fs.mkdirSync(dirname, "0664");
+            fs.writeFileSync(pathname, content)
+        }
+    } catch(e) {
+        var error = parseError(e);
+        respond(res, error.code, error.msg);        
+        return;
     }
+    respond(res, 200, "OK");
 }
 
 function deleteContent(pathname) {
-   fs.unlinkSync(pathname); 
+    try {
+        fs.unlinkSync(pathname); 
+    } catch (e) {
+        var error = parseError(e);
+        respond(res, error.code, error.msg);        
+        return;
+    }
+    respond(res, 200, "OK");
+}
+
+function respond(res, msg, statusCode) {
+    res.writeHead(statusCode, HTTPStatus[statusCode]);
+    res.write(new Buffer(msg));
+    res.end();
+}
+
+function parseError(error) {
+    var msg = error.message;
+    var re = /(EACCES|ENOENT)/;
+    var codeMap = {
+        "EACCES": 403,
+        "ENOENT": 503,
+    }
+    var matches = re.exec(msg);
+    if (!matches) {
+        return {
+            msg: HTTPStatus[500],
+            code: 500
+        }
+    }
+    return {
+        msg: msg,
+        code: codeMap[matches[1]]
+    };
 }
 
 http.createServer(function (req, res) {
     var data = [];
     var params = parseUrl(req.url);
     if (!params.offset) {
-        console.log("url error");
+        respond(res, 400, "url wrong");
         return;
     }
     req.on("data", function (chunk) {
@@ -47,12 +95,10 @@ http.createServer(function (req, res) {
         var offset = data.readUIntLE(0, 2);
         var pathName = data.slice(2, 2 + offset).toString();
         switch(params.status) {
-            case 'M': modifyContent(pathName, data.slice(2 + offset));break;
-            case 'A': addContent(pathName, data.slice(2 + offset));break;
-            case 'D': deleteContent(pathName);break;
+            case 'M': modifyContent(res, pathName, data.slice(2 + offset));break;
+            case 'A': addContent(res, pathName, data.slice(2 + offset));break;
+            case 'D': deleteContent(res, pathName);break;
         }
-        res.writeHead(200, "OK");
-        res.end();
     })
 
 }).listen(18088, '127.0.0.1');
